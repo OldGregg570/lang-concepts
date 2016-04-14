131865e4e2e128cdcd736cdc5da11fda540ff825
Andrew Bragg <andrew.bragg@autodesk.com>
2016-04-14 01:19:28 -0700

    Styling assertion error

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,10 @@
 #include<stdio.h>
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-#define SUCCESS_STRING     ANSI_COLOR_GREEN "All tests pass!\n" ANSI_COLOR_RESET
-#define ERROR_STRING       ANSI_COLOR_RED "Error: %d asserton%c failed\n" ANSI_COLOR_RESET
+#define COLOR_RED         "\x1b[31m"
+#define COLOR_GREEN       "\x1b[32m"
+#define COLOR_RESET       "\x1b[0m"
+#define SUCCESS_STRING    COLOR_GREEN "All tests pass!\n" COLOR_RESET
+#define ERROR_STRING      COLOR_RED "Error: %d asserton%c failed\n" COLOR_RESET
+#define ASSERT_ERR_STRING COLOR_RED "Assertion Failure: %d == %d\n" COLOR_RESET
 
 int assertIntEquals (int, int);
 
@@ -12,7 +13,7 @@ int assertIntEquals (int, int);
  */
 int assertIntEquals (int left, int right) {
  if (left != right) {
-  printf("Assertion Failure: %d == %d\n", left, right);
+  printf(ASSERT_ERR_STRING, left, right);
  }
  return left != right;
 }
