8dba2d12c8d536a55854c790f16e9d61a2e1b65e
Andrew Bragg <andrew.bragg@autodesk.com>
2016-04-13 22:49:49 -0700

    Returning 1 if assertion fails

--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void assertIntEquals (int, int);
+int assertIntEquals (int, int);
 
 /**
  * Print an error message if left does not equal right
  */
-void assertIntEquals (int left, int right) {
- if (left != right) printf("Assertion Failure: %d == %d\n", left, right);
+int assertIntEquals (int left, int right) {
+ if (left != right) {
+  printf("Assertion Failure: %d == %d\n", left, right);
+ }
+ return left != right;
 }
